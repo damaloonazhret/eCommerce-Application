78433422a9e3303367feda57999ef3cce69a9a21
Denys Bilonozhko <deformix@gmail.com>
2023-08-01 20:33:32 +0300

    chore: RE-31 add prettier config

--- src/components/app/index.ts
+++ src/components/app/index.ts
@@ -1,13 +1,13 @@
 import img from '../../assets/images/html.png';
 
 export default class App {
-  private root: HTMLElement;
+    private root: HTMLElement;
 
-  constructor(root: HTMLElement) {
-    this.root = root;
-  }
+    constructor(root: HTMLElement) {
+        this.root = root;
+    }
 
-  public start(): void {
-    this.root.innerHTML = `<img src="${img}" alt="html" />`;
-  }
+    public start(): void {
+        this.root.innerHTML = `<img src="${img}" alt="html" />`;
+    }
 }

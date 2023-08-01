1f2de589999d024f414d21de25c5b3438f9da9c2
Denys Bilonozhko <deformix@gmail.com>
2023-08-01 20:23:17 +0300

    chore: RE-30 set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    ignorePatterns: ['node_module', 'dist', '.eslintrc.js', 'webpack.config.js', 'webpack.dev.config.js', 'webpack.prod.config.js'],
+    extends: [
+        'airbnb-base',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'airbnb-typescript/base',
+    ],
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json',
+    },
+    plugins: ['@typescript-eslint'],
+    root: true,
+    rules: {
+        'import/no-cycle': 'off',
+        'no-void': 'off',
+        '@typescript-eslint/no-misused-promises': 'off',
+        'import/extensions': 'off',
+        'no-debugger': 'off',
+        'no-console': 0,
+        'class-methods-use-this': 'off',
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'error',
+        '@typescript-eslint/explicit-member-accessibility': [
+            'error',
+            {
+                accessibility: 'explicit',
+                overrides: {
+                    accessors: 'explicit',
+                    constructors: 'off',
+                    methods: 'explicit',
+                    properties: 'explicit',
+                    parameterProperties: 'explicit',
+                },
+            },
+        ],
+    },
+};

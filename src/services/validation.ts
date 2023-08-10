e5f41f95c88909188107b077fff5fe4e1054873f
p0lluxstar <web-registra@yandex.ru>
2023-08-10 17:15:33 +0300

    feat: add test validation data form login and registration

--- src/services/validation.ts
+++ src/services/validation.ts
@@ -1,4 +1,4 @@
-export function checkDataLoginForm(emailUser: string, passwordUser: string): void {
+export function checkDataLoginForm(emailUser: string, passwordUser: string): string {
     // check email, <input> type='text'
     const hasUpperCaseEmail = /[A-Z]/.test(emailUser);
     const hasSpacesEmail = /^\s|\s$/.test(emailUser);
@@ -7,18 +7,22 @@ export function checkDataLoginForm(emailUser: string, passwordUser: string): voi
 
     if (hasUpperCaseEmail) {
         console.log('err-login: has uppercase in email');
+        return 'Incorrect data';
     }
 
     if (hasSpacesEmail) {
         console.log('err-login: has space in email');
+        return 'Incorrect data';
     }
 
     if (!hasSymbolDogEmail) {
         console.log('err-login: no symbol @ in email');
+        return 'Incorrect data';
     }
 
     if (!hasDomenEmail) {
         console.log('err-login: no domen in email');
+        return 'Incorrect data';
     }
 
     // check password, <input> type='password'
@@ -31,27 +35,35 @@ export function checkDataLoginForm(emailUser: string, passwordUser: string): voi
 
     if (passwordUser.length < minLengthPassword) {
         console.log('err-login: wrong length password');
+        return 'Incorrect data';
     }
 
     if (!hasUpperCasePassword) {
         console.log('err-login: no uppercase symbol in password');
+        return 'Incorrect data';
     }
 
     if (!hasLowerCasePassword) {
         console.log('err-login: no lowercase symbol in password');
+        return 'Incorrect data';
     }
 
     if (!hasNumberPassword) {
         console.log('err-login: no number in password');
+        return 'Incorrect data';
     }
 
     if (!hasSpecialSymbolPassword) {
         console.log('err-login: no special symbol in password');
+        return 'Incorrect data';
     }
 
     if (hasSpacesPassword) {
         console.log('err-login: has space in password');
+        return 'Incorrect data';
     }
+
+    return 'Correct data';
 }
 
 export function checkDataRegistrationForm(
@@ -64,14 +76,17 @@ export function checkDataRegistrationForm(
     addressCity: string,
     adressPostalCode: string,
     adressCountry: string
-): void {
-    checkDataLoginForm(emailUser, passwordUser);
+): string {
+    if (checkDataLoginForm(emailUser, passwordUser) === 'Incorrect data') {
+        return 'Incorrect data';
+    }
 
     // check data registration
     const minQuantitySymbol = 1;
     const hasSpecialSymbolDataName = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?\d]/.test(nameUser);
     const hasSpecialSymbolDataCity = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?\d]/.test(addressCity);
     const dateBirthUser = new Date(dateBirth).getTime();
+    console.log(dateBirthUser);
     const minAgeUser = 10;
     const yearMaxDateAge = Number(new Date().getFullYear()) - minAgeUser;
     const monthMaxDateAge = `0${new Date().getMonth() + 1}`.slice(-2);
@@ -81,31 +96,40 @@ export function checkDataRegistrationForm(
 
     if (nameUser.length < minQuantitySymbol || lastNameUser.length < minQuantitySymbol) {
         console.log('err-reg: wrong length name or wrong length lastname');
+        return 'Incorrect data';
     }
 
     if (hasSpecialSymbolDataName) {
         console.log('err-reg: has spec symbol or number in name');
+        return 'Incorrect data';
     }
 
     // birth day <input> type='date'
     if (dateBirthUser > maxDateAge || !dateBirthUser) {
         console.log('err-reg: you less then 10 years old');
+        return 'Incorrect data';
     }
 
     if (addressStreet.length < minQuantitySymbol) {
         console.log('err-reg: wrong adress street');
+        return 'Incorrect data';
     }
 
     if (addressCity.length < minQuantitySymbol || hasSpecialSymbolDataCity) {
         console.log('err-reg: wrong adress city');
+        return 'Incorrect data';
     }
 
     if (!hasNumberSymbolUppercasePostalCode) {
         console.log('err-reg: wrong postal code');
+        return 'Incorrect data';
     }
 
     // country <select>
     if (adressCountry.length === 0) {
         console.log('err-reg: no select country');
+        return 'Incorrect data';
     }
+
+    return 'Correct data';
 }
---
+++ src/tests/validation.test.ts
@@ -0,0 +1,193 @@
+import { checkDataLoginForm, checkDataRegistrationForm } from '../services/validation';
+
+// one metod
+/* test('checking form login', () => {
+    expect(checkDataLoginForm('test@email.ru', '1!Atestpassword')).toBe('Successfully');
+}); */
+
+describe('Checking form login', () => {
+    const testCase = [
+        {
+            email: '1@mail.ru',
+            password: 'Aqswdefr1!',
+            expected: 'Correct data',
+        },
+        {
+            email: 'test@email.ru ',
+            password: 'Aqswdefr1!',
+            expected: 'Incorrect data',
+        },
+        {
+            email: ' test@email.ru',
+            password: 'Aqswdefr1!',
+            expected: 'Incorrect data',
+        },
+        {
+            email: 'test@email.ru',
+            password: 'qswdefre',
+            expected: 'Incorrect data',
+        },
+        {
+            email: 'test@email.ru',
+            password: 'qswdefre1',
+            expected: 'Incorrect data',
+        },
+        {
+            email: 'test@email.ru',
+            password: 'qswdefre1!',
+            expected: 'Incorrect data',
+        },
+        {
+            email: '',
+            password: '',
+            expected: 'Incorrect data',
+        },
+    ];
+    testCase.forEach((test) => {
+        it(`data: ${test.email}, ${test.password}, expected:${test.expected}`, () => {
+            const res = checkDataLoginForm(test.email, test.password);
+            expect(res).toBe(test.expected);
+        });
+    });
+});
+
+describe('Checking form registration', () => {
+    const testCase = [
+        {
+            email: 'test@email1.ru',
+            password: 'Aqswdefr1!',
+            name: 'Jonh',
+            lastName: 'Smith',
+            datebirth: '2010-01-01',
+            street: 'Brooklyn',
+            city: 'New York',
+            postCode: '123A15',
+            country: 'USA',
+            expected: 'Correct data',
+        },
+        {
+            email: ' test@email2.ru',
+            password: 'Aqswdefr1!',
+            name: 'Jonh',
+            lastName: 'Smith',
+            datebirth: '2010-01-01',
+            street: 'Brooklyn',
+            city: 'New York',
+            postCode: '123A15',
+            country: 'USA',
+            expected: 'Incorrect data',
+        },
+        {
+            email: 'test@email3.ru',
+            password: 'password1',
+            name: '',
+            lastName: 'Smith',
+            datebirth: '2010-01-01',
+            street: 'Brooklyn',
+            city: 'New York',
+            postCode: '123A15',
+            country: 'USA',
+            expected: 'Incorrect data',
+        },
+        {
+            email: 'test@email4.ru',
+            password: 'Aqswdefr1!',
+            name: 'Jonh',
+            lastName: '',
+            datebirth: '2010-01-01',
+            street: 'Brooklyn',
+            city: 'New York',
+            postCode: '123A15',
+            country: 'USA',
+            expected: 'Incorrect data',
+        },
+        {
+            email: 'test@email5.ru',
+            password: 'Aqswdefr1!',
+            name: 'Jonh',
+            lastName: 'Smith',
+            datebirth: '2020-01-01',
+            street: 'Brooklyn',
+            city: 'New York',
+            postCode: '123A15',
+            country: 'USA',
+            expected: 'Incorrect data',
+        },
+        {
+            email: 'test@email6.ru',
+            password: 'password',
+            name: 'Jonh',
+            lastName: 'Smith',
+            datebirth: '2010-01-01',
+            street: 'Brooklyn',
+            city: 'New York',
+            postCode: '123A15',
+            country: 'USA',
+            expected: 'Incorrect data',
+        },
+        {
+            email: 'test@email7.ru',
+            password: 'Aqswdefr1!',
+            name: 'Jonh',
+            lastName: 'Smith',
+            datebirth: '2010-01-01',
+            street: '',
+            city: 'New York',
+            postCode: '123A15',
+            country: 'USA',
+            expected: 'Incorrect data',
+        },
+        {
+            email: 'test@email8.ru',
+            password: 'Aqswdefr1!',
+            name: 'Jonh',
+            lastName: 'Smith',
+            datebirth: '2010-01-01',
+            street: 'Brooklyn',
+            city: '',
+            postCode: '123A15',
+            country: 'USA',
+            expected: 'Incorrect data',
+        },
+        {
+            email: 'test@email9.ru',
+            password: 'Aqswdefr1!',
+            name: 'Jonh',
+            lastName: 'Smith',
+            datebirth: '2010-01-01',
+            street: 'Brooklyn',
+            city: 'New York',
+            postCode: '',
+            country: 'USA',
+            expected: 'Incorrect data',
+        },
+        {
+            email: 'test@email10.ru',
+            password: 'Aqswdefr1!',
+            name: 'Jonh',
+            lastName: 'Smith',
+            datebirth: '2010-01-01',
+            street: 'Brooklyn',
+            city: 'New York',
+            postCode: '123A15',
+            country: '',
+            expected: 'Incorrect data',
+        },
+    ];
+    testCase.forEach((test) => {
+        it(`data: ${test.email}`, () => {
+            const res = checkDataRegistrationForm(
+                test.email,
+                test.password,
+                test.name,
+                test.lastName,
+                test.datebirth,
+                test.street,
+                test.city,
+                test.postCode,
+                test.country
+            );
+            expect(res).toBe(test.expected);
+        });
+    });
+});

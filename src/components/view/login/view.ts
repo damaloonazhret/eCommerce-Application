5dcb52fc41636b4e3b3bf631b945fd1220c17e99
Andrey <84859209+damaloonazhret@users.noreply.github.com>
2023-08-12 11:47:23 +0300

    feat: input div container, input id

--- src/components/view/login/view.ts
+++ src/components/view/login/view.ts
@@ -1,15 +1,26 @@
 export default class InputGenerator {
+    private readonly inputContainer: HTMLDivElement;
+
     private readonly input: HTMLInputElement;
 
-    constructor(type: string, placeholder: string, className: string) {
-        this.input = this.createInput(type, placeholder, className);
+    constructor(type: string, placeholder: string, className: string, id: string) {
+        this.inputContainer = this.createInputContainer(className);
+        this.input = this.createInput(type, placeholder, id);
+        this.inputContainer.appendChild(this.input);
+        this.inputContainer.appendChild(document.createElement('span'));
     }
 
-    private createInput(type: string, placeholder: string, className: string): HTMLInputElement {
+    private createInputContainer(className: string): HTMLDivElement {
+        const container = document.createElement('div');
+        container.classList.add(className);
+        return container;
+    }
+
+    private createInput(type: string, placeholder: string, id: string): HTMLInputElement {
         const input = document.createElement('input');
         input.type = type;
         input.placeholder = placeholder;
-        input.classList.add(className);
+        input.id = id;
         return input;
     }
 
@@ -22,8 +33,8 @@ export default class InputGenerator {
         return button;
     }
 
-    public getInput(): HTMLInputElement {
-        return this.input;
+    public getInputContainer(): HTMLDivElement {
+        return this.inputContainer;
     }
 
     public getButton(id: string, text: string, clickHandler: () => void): HTMLButtonElement {
@@ -31,36 +42,41 @@ export default class InputGenerator {
     }
 }
 
-export const loginInput = new InputGenerator('text', 'Enter Email', 'login__email');
-export const passwordInput = new InputGenerator('password', 'Enter password', 'login__password');
+export const loginInput = new InputGenerator('text', 'Enter Email', 'login__email', 'email');
+export const passwordInput = new InputGenerator('password', 'Enter password', 'login__password', 'password');
 export const loginForm = document.createElement('form');
 
-const buttonGenerator = new InputGenerator('button', 'Button Text', 'login__button');
-const buttonGeneratorReg = new InputGenerator('button', 'Button Text', 'reg__button');
+const buttonGenerator = new InputGenerator('button', 'Button Text', 'login__button', 'login-btn');
+const buttonGeneratorReg = new InputGenerator('button', 'Button Text', 'reg__button', 'reg-btn');
 
-loginForm.appendChild(loginInput.getInput());
-loginForm.appendChild(passwordInput.getInput());
+loginForm.appendChild(loginInput.getInputContainer());
+loginForm.appendChild(passwordInput.getInputContainer());
 loginForm.appendChild(buttonGenerator.getButton('login__button', 'LOGIN', () => {}));
 
-export const emailInputReg = new InputGenerator('email', 'Email', 'registration__input-email');
-export const passwordInputReg = new InputGenerator('password', 'Password', 'registration__input-password');
-export const firstNameInputReg = new InputGenerator('text', 'First name', 'registration__input-first');
-export const lastNameInputReg = new InputGenerator('text', 'Last name', 'registration__input-last');
-export const dobInputReg = new InputGenerator('date', 'Date of birth', 'registration__input-dob');
-export const streetInputReg = new InputGenerator('text', 'Street', 'registration__input-street');
-export const cityInputReg = new InputGenerator('text', 'City', 'registration__input-city');
-export const postalCodeInputReg = new InputGenerator('text', 'Postal code', 'registration__input-postal');
-export const countryInputReg = new InputGenerator('text', 'Country', 'registration__input-country');
+export const emailInputReg = new InputGenerator('email', 'Email', 'registration__input-email', 'email-reg');
+export const passwordInputReg = new InputGenerator(
+    'password',
+    'Password',
+    'registration__input-password',
+    'password-reg'
+);
+export const firstNameInputReg = new InputGenerator('text', 'First name', 'registration__input-first', 'first-name');
+export const lastNameInputReg = new InputGenerator('text', 'Last name', 'registration__input-last', 'last-name');
+export const dobInputReg = new InputGenerator('date', 'Date of birth', 'registration__input-dob', 'dob');
+export const streetInputReg = new InputGenerator('text', 'Street', 'registration__input-street', 'street');
+export const cityInputReg = new InputGenerator('text', 'City', 'registration__input-city', 'city');
+export const postalCodeInputReg = new InputGenerator('text', 'Postal code', 'registration__input-postal', 'postal');
+export const countryInputReg = new InputGenerator('text', 'Country', 'registration__input-country', 'country');
 
 export const registrationInput = document.createElement('form');
 
-registrationInput.appendChild(emailInputReg.getInput());
-registrationInput.appendChild(passwordInputReg.getInput());
-registrationInput.appendChild(firstNameInputReg.getInput());
-registrationInput.appendChild(lastNameInputReg.getInput());
-registrationInput.appendChild(dobInputReg.getInput());
-registrationInput.appendChild(streetInputReg.getInput());
-registrationInput.appendChild(cityInputReg.getInput());
-registrationInput.appendChild(postalCodeInputReg.getInput());
-registrationInput.appendChild(countryInputReg.getInput());
+registrationInput.appendChild(emailInputReg.getInputContainer());
+registrationInput.appendChild(passwordInputReg.getInputContainer());
+registrationInput.appendChild(firstNameInputReg.getInputContainer());
+registrationInput.appendChild(lastNameInputReg.getInputContainer());
+registrationInput.appendChild(dobInputReg.getInputContainer());
+registrationInput.appendChild(streetInputReg.getInputContainer());
+registrationInput.appendChild(cityInputReg.getInputContainer());
+registrationInput.appendChild(postalCodeInputReg.getInputContainer());
+registrationInput.appendChild(countryInputReg.getInputContainer());
 registrationInput.appendChild(buttonGeneratorReg.getButton('registration__button', 'REGISTRATION', () => {}));

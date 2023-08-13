e62eb94f0c0d177b595f618f74e5fcbfede92126
Denys Bilonozhko <deformix@gmail.com>
2023-08-13 16:20:06 +0300

    feat: add placeholder pages: about, contact

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,8 +1,10 @@
+import About from '../components/view/about';
+import Contact from '../components/view/contact';
 import Login from '../components/view/login';
 import Registration from '../components/view/registration';
 import Shop from '../components/view/shop';
 
 export default interface Route {
     path: string;
-    View: typeof Shop | typeof Login | typeof Registration;
+    View: typeof Shop | typeof Login | typeof Registration | typeof About | typeof Contact;
 }

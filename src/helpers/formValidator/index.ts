01593183c13da58533212d8f5e64890e99c50691
Andrey <84859209+damaloonazhret@users.noreply.github.com>
2023-08-15 21:37:36 +0300

    feat: a new way to display errors, all errors at once, instead of one

--- src/helpers/formValidator/index.ts
+++ src/helpers/formValidator/index.ts
@@ -1,15 +1,23 @@
 export default class FormValidator {
     public static handleValidation(inputContainer: HTMLElement, isValid: string[], span: HTMLSpanElement): void {
+        const newSpan = document.createElement('span');
+
         if (isValid.length === 0) {
             inputContainer.classList.remove('error');
         } else {
             inputContainer.classList.add('error');
             if ('innerHTML' in span) {
-                const [firstIsValid] = isValid;
-                const newSpan = document.createElement('span');
-                newSpan.innerHTML = firstIsValid;
-                span.parentNode?.replaceChild(newSpan, span);
+                const errorList = document.createElement('ul');
+                isValid.forEach((errorMessage) => {
+                    const errorItem = document.createElement('li');
+                    errorItem.textContent = errorMessage;
+                    errorList.appendChild(errorItem);
+                });
+
+                newSpan.appendChild(errorList);
             }
         }
+
+        span.parentNode?.replaceChild(newSpan, span);
     }
 }

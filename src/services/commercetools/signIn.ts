38864ce738bfd272f56ec513703e29d1c072da5b
Denys Bilonozhko <deformix@gmail.com>
2023-08-27 18:28:54 +0300

    chore: add env

--- src/services/commercetools/signIn.ts
+++ src/services/commercetools/signIn.ts
@@ -1,15 +1,17 @@
 import { AccessTokenResponse, Customer, UserLoginData } from '../../types/interfaces';
-import { CTP_API_URL, CTP_PROJECT_KEY } from './credential';
 
 async function signIn(token: string, userData: UserLoginData): Promise<AccessTokenResponse | Customer> {
-    const response = await fetch(`${CTP_API_URL}/${CTP_PROJECT_KEY}/login`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(userData),
-    });
+    const response = await fetch(
+        `${process.env.CTP_API_URL as string}/${process.env.CTP_PROJECT_KEY as string}/login`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify(userData),
+        }
+    );
 
     if (!response.ok) {
         const result = (await response.json()) as AccessTokenResponse;
